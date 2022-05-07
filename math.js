44a50b7621506daf2c4a4ab1f89018ec524b2541
nazmul33 <nazmulnahid73@gmail.com>
2022-05-07 20:15:24 +0600

    java

---
+++ math.js
@@ -0,0 +1,7 @@
+// const myNumber = 21.95;
+// const output = Math.random(myNumber);
+// console.log(output);
+
+const output = Math.random() * 10;
+const rounded = Math.floor(output);
+console.log(rounded);
---
+++ random.js
@@ -0,0 +1,3 @@
+const output = Math.random() * 10;
+const rounded = Math.floor(output);
+console.log(rounded);
